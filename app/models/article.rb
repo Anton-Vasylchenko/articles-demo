838f43c27bdc2129410f3e200cd038b77b5aaa44
Anton Vasylchenko <anton.vasylchenko.v@gmail.com>
2022-10-11 16:51:41 +0300

    add text editor

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,9 @@
 class Article < ApplicationRecord
     include ImageUploader::Attachment(:image)
-    
+
+    has_rich_text :description
+
     belongs_to :user
     validates :title, presence: true, length: { minimum: 6, maximum: 100 }
-    validates :description, presence: true, length: { minimum: 10, maximum: 1000 }
+    validates :description, presence: true, length: { minimum: 10 }
 end
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,5 @@ pin "popper", to: 'popper.js', preload: true
 pin "bootstrap", to: 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js', preload: true
 pin_all_from "app/javascript/custom", under: "custom"
 pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.0.4/lib/assets/compiled/rails-ujs.js"
+pin "trix"
+pin "@rails/actiontext", to: "actiontext.js"
